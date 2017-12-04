de78a02f9db6361fa7a898a919855f9f75010483
Richard Zhang <richardzcode@gmail.com>
2017-12-04 15:05:04 -0800

    Device

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Url = exports.JS = exports.Device = exports.Logger = undefined;
 
 var _logger = require('./logger');
 
@@ -14,12 +13,12 @@ Object.defineProperty(exports, 'Logger', {
   }
 });
 
-var _device = require('./device');
+var _Device = require('./Device');
 
 Object.defineProperty(exports, 'Device', {
   enumerable: true,
   get: function get() {
-    return _interopRequireDefault(_device).default;
+    return _interopRequireDefault(_Device).default;
   }
 });
 
@@ -40,9 +39,5 @@ Object.defineProperty(exports, 'Url', {
     return _interopRequireDefault(_Url).default;
   }
 });
-
-var Device = _interopRequireWildcard(_device);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import * as Device from './device';
-
 export { ConsoleLogger as Logger } from './logger';
-export { default as Device } from './device';
+export { default as Device } from './Device';
 export { default as JS } from './JS';
 export { default as Url } from './Url';
